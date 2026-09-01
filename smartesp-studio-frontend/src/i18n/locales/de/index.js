7ee6225ec5b28f72de5c181f791f0ed987439bcf
Jan <jan.hoffmeyer@ceiton.com>
2026-09-01 21:18:23 +0200

    i18n: schema.json-Katalog (LVGL default_font/theme/disp_* + Style-Props) + common.help

--- smartesp-studio-frontend/src/i18n/locales/de/index.js
+++ smartesp-studio-frontend/src/i18n/locales/de/index.js
@@ -2,5 +2,6 @@ import app from "./app.json";
 import common from "./common.json";
 import builder from "./builder.json";
 import modals from "./modals.json";
+import schema from "./schema.json";
 
-export default { app, common, builder, modals };
+export default { app, common, builder, modals, schema };
